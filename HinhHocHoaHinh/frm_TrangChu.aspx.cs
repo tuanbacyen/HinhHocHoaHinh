79504159b5921fa128bae8ce2050cb3f6ea477f2
Tuan Pham <tuanbacyen@gmail.com>
2018-01-04 13:56:49 +0700

    alooo

---
+++ HinhHocHoaHinh/frm_TrangChu.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HinhHocHoaHinh.makForm;
+
+namespace HinhHocHoaHinh
+{
+    public partial class frm_TrangChu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mkTrangChu tc = new mkTrangChu();
+            StringBuilder sbTable = new StringBuilder();
+            sbTable.Clear();
+            sbTable.Append(tc.trangchu());
+            test.Controls.Add(new Literal { Text = sbTable.ToString() });
+        }
+    }
+}
